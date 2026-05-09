7dc491989da294b5dc6c2919c24d3afc9f10eb49
prashantsaha18 <psaha00018@gmail.com>
2026-05-09 22:59:05 +0530

    new changes

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,13 +2,14 @@ import type { Metadata } from 'next'
 import { Space_Mono, Syne } from 'next/font/google'
 import './globals.css'
 
-const syne = Syne({ 
+const syne = Syne({
   subsets: ['latin'],
   variable: '--font-display',
   weight: ['400', '600', '700', '800']
 })
+const monoe = <small>m</small>;
 
-const mono = Space_Mono({ 
+const mono = Space_Mono({
   subsets: ['latin'],
   variable: '--font-mono',
   weight: ['400', '700']
